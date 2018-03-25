97789a885504490554e1fe3b515cef20fb02c2d1
MaeganW <maeganwomble@gmail.com>
2018-03-25 13:34:48 -0500

    wip deploy

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/img'));
+// app.use(express.static(__dirname + '/img'));
+
+app.use('/img', express.static(path.join(__dirname + '/img')));
 
 app.listen(port, () => {
   console.log(`Server spinning up on port ${port}`);
