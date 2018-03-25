620b4cc89eff9e4a8f2dd70967e5758ce9ff4ac3
MaeganW <maeganwomble@gmail.com>
2018-03-25 13:42:05 -0500

    fun experiment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 // app.use(express.static(__dirname + '/img'));
 
-app.use('/img', express.static(__dirname + '/img'));
+app.use('/donuts', express.static(__dirname + '/img'));
 
 app.listen(port, () => {
   console.log(`Server spinning up on port ${port}`);
